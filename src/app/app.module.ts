03b997aaf489e496bc36c230790549c2d65238dc
NahuelGallotti <nahuelgallotti190300@gmail.com>
2020-05-30 17:39:21 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+
+const routes: Route[] = [
+ 
+  {path: 'contacto', component: ContactoComponent},
+
+   /*Pagina Principal porue no tiene ruta*/
+  {path: '', component: PaginaprincipalComponent},
+];
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MenuComponent } from './menu/menu.component';
@@ -9,15 +18,14 @@ import { ContactoComponent } from './contacto/contacto.component';
 
 /*Enrutadores de Navegacion*/
 import { RouterModule, Route } from "@angular/router";
+import { PaginaprincipalComponent } from './paginaprincipal/paginaprincipal.component';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MenuComponent, ContactoComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  declarations: [ AppComponent, HelloComponent, MenuComponent, ContactoComponent, PaginaprincipalComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
 
-const routes: Route[] = [
- 
-  
-];
